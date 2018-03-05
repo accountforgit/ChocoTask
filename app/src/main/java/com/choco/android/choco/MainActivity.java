e3f5305f2aef1a58afd1fad75db65f6ec34670d6
nurzhigit.shalkarbai@ce.sdu.edu.kz <asd>
2018-03-05 21:23:17 +0600

    Launching at 21:24 10-9-8-7-6-5-4-3-2-1-

---
+++ app/src/main/java/com/choco/android/choco/MainActivity.java
@@ -0,0 +1,13 @@
+package com.choco.android.choco;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
